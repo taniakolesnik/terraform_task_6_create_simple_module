5589d58a6b4e66ac9c1aa939322bf2fb6260d7ca
Tania Kolesnik <kolesnik@gmail.com>
2026-05-17 18:32:04 +0100

    add solution

--- modules/resource_group_storage/variables.tf
+++ modules/resource_group_storage/variables.tf
@@ -1,15 +1,15 @@
 variable "resource_group_name" {
   description = "The name of the resource group"
-  default = "test-rg"
+  default     = "test-rg"
 }
 
 variable "location" {
   description = "The name of the storage account"
-  default = "East US"
+  default     = "East US"
 }
 
 variable "storage_account_name" {
   description = "The name of the storage account"
-  default = "test-sa"
+  default     = "test-sa"
 }
 
